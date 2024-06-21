bfc43b8bb7eab36c233d1c000312e2a02262e1fb
Kheneahm Ares <kheneahm9@gmail.com>
2024-06-20 21:49:53 -0500

    add basic and temp crud methods for auctions controller

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -40,7 +40,82 @@ public class AuctionsController : ControllerBase
         {
             return NotFound();
         }
-        
+
         return _mapper.Map<AuctionDto>(auction);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<AuctionDto>> Post([FromBody] CreateAuctionDto dto)
+    {
+        var auction = _mapper.Map<Auction>(dto);
+        auction.Seller = "test";
+        _dbContext.Auctions.Add(auction);
+        var result = await _dbContext.SaveChangesAsync();
+        if (result == 0)
+        {
+            return BadRequest();
+        }
+        else
+        {
+            //show location in header of where it's created
+            return CreatedAtAction(nameof(Get), new { id = auction.Id }, _mapper.Map<AuctionDto>(auction));
+        }
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<AuctionDto>> Put(Guid id, [FromBody] UpdateAuctionDto dto)
+    {
+        var auction = await _dbContext.Auctions
+            .Include(x => x.Item)
+            .FirstOrDefaultAsync(x => x.Id == id);
+        if (auction == null)
+        {
+            return NotFound();
+        }
+
+        //TODO: check if user is the seller
+
+        auction.Item.Make = dto.Make ?? auction.Item.Make;
+        auction.Item.Model = dto.Model ?? auction.Item.Model;
+        auction.Item.Mileage = dto.Mileage ?? auction.Item.Mileage;
+        auction.Item.Color = dto.Color ?? auction.Item.Color;
+
+        var result = await _dbContext.SaveChangesAsync();
+        if (result == 0)
+        {
+            return BadRequest();
+        }
+        else
+        {
+            return Ok(_mapper.Map<AuctionDto>(auction));
+        }
+    }
+
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(Guid id)
+    {
+        var auction = await _dbContext.Auctions
+            .Include(x => x.Item)
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+        if (auction == null)
+        {
+            return NotFound();
+        }
+
+        //todo: check if user is the seller
+
+        
+        _dbContext.Auctions.Remove(auction);
+        var result = await _dbContext.SaveChangesAsync();
+        if (result == 0)
+        {
+            return BadRequest();
+        }
+        else
+        {
+            return Ok();
+        }
     }
 }

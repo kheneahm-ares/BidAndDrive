f0eddb81a3b315872732d47d0c3fe79d13fc3ce0
Kheneahm Ares <kheneahm9@gmail.com>
2024-06-22 19:25:19 -0500

    seeding data in mongodb

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,8 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Data;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -7,5 +12,14 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    await DbInitializer.InitAsync(app);
+}
+catch
+{
+    Console.WriteLine("Could not seed database");
+}
 
 app.Run();
